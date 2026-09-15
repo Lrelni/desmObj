54f1075c9c7030fa7188d4d5cce18d36aab7ea00
Lrelni <lrelni.official@gmail.com>
2026-09-14 21:41:15 -0600

    Refactor desmObj.py for better functionality

--- src/desmObj.py
+++ src/desmObj.py
@@ -1,6 +1,7 @@
 # desmObj: obj file format to Desmos vertex lists.
 # vertex colors will be interpolated for their faces
 import sys
+from pathlib import Path
 
 # helper function to process face entries
 def fp(s):
@@ -16,31 +17,50 @@ def name_parse(name):
     else:
         return name[0]+"_{"+name[1:]+"}"
 
-def main(args):
-    OBJECT_NAME = "object"
-    OUTPUT_NAME = "output"
-    FLIP_AXES = (-1,1,1)
-    # subscripts used: v f c
+def sanitize(x):
+    return "".join(c for c in x if c.isalnum())
 
-    
-    
-    obj_file = open(args[1])
-    output_file = open(OUTPUT_NAME + ".txt", "w")
+def flatten(x):
+    # flatten list of strings
+    result = "["
+    for s in x:
+        result += s+","
+    result = result[:-1] + "]"
+    return result
+
+def main(args):
+    input_file = open(args[1])
+    input_name = Path(input_file.name).stem
+
+    output_name = input_name
+    output_file = open(output_name + ".txt", "w")
+
+    obj_name = sanitize(input_name)
+    FLIP_AXES = (1,1,1)
+    RENDER_HELPER = True
+
     # all colors given in (R, G, B) in the range [0, 1]
     vertices = [] # [ (float, float, float) ... ]
+    faces = [] # [ (int, int, int) ... ]
     vertex_colors = [] # [ (float, float, float) ... ]
     face_colors = [] # [ (float, float, float) ... ]
-    faces = [] # [ (int, int, int) ... ]
-    for line in obj_file:
+
+
+    colors_enabled = False # default to false and change to true later if needed
+
+    for line in input_file:
         entry = line.split()
         match entry[0]:
             case 'v':
                 vertices.append((FLIP_AXES[0]*float(entry[1]),\
                                 FLIP_AXES[1]*float(entry[2]),\
                                 FLIP_AXES[2]*float(entry[3])))
-                vertex_colors.append((float(entry[4]),\
-                                    float(entry[5]),\
-                                    float(entry[6])))
+                if len(entry) > 4:
+                    colors_enabled = True
+                if colors_enabled:
+                    vertex_colors.append((float(entry[4]),\
+                                        float(entry[5]),\
+                                        float(entry[6])))
             case 'f':
                 faces.append((fp(entry[1]),\
                             fp(entry[2]),\
@@ -48,46 +68,45 @@ def main(args):
             case _:
                 pass
 
-    # desmos does not support varied colors per polygon,
-    # so vertex colors are averaged into a face color
-    for face in faces:
-        face_color = avg(vertex_colors[face[0]-1],\
-                          vertex_colors[face[1]-1],\
-                              vertex_colors[face[2]-1])
-        face_colors.append(face_color)
+    if colors_enabled:
+        # desmos does not support varied colors per polygon,
+        # so vertex colors are averaged into a face color
+        for face in faces:
+            face_colors.append(avg(vertex_colors[face[0]-1],\
+                            vertex_colors[face[1]-1],\
+                            vertex_colors[face[2]-1]))
     
 
-    result_dict = {"v" : name_parse(OBJECT_NAME+"v")+"=\\left[",
-                   "f" : name_parse(OBJECT_NAME+"f")+"=\\left[",
-                   "c" : name_parse(OBJECT_NAME+"c")+"=\\left["}
-    
-    for vertex in vertices:
-        # format() used to avoid str() making, for exmaple, 1e-06
-        # which would mess up the stringifying
-        result_dict["v"] += "\\left("+format(vertex[0], "f")+","\
+    result_dict = {"v" : name_parse(obj_name+"v")+"=",
+                   "f" : name_parse(obj_name+"f")+"=",}
+
+    result_dict["v"] += flatten("("+format(vertex[0], "f")+","\
                          + format(vertex[1], "f") + ","\
-                         + format(vertex[2], "f") + "\\right),"
+                         + format(vertex[2], "f") + ")" for vertex in vertices)
 
-    # remove the last comma from the string and add ending bracket
-    result_dict["v"] = result_dict["v"][:-1] + "\\right]"
-
-    for face in faces:
-        result_dict["f"] += "\\left("+str(face[0]) + ","\
+    result_dict["f"] += flatten("("+str(face[0]) + ","\
                          + str(face[1]) + ","\
-                         + str(face[2]) + "\\right),"
+                         + str(face[2]) + ")" for face in faces)
 
-    result_dict["f"] = result_dict["f"][:-1] + "\\right]"
-    
-    for color in face_colors:
-        result_dict["c"] += "\\operatorname{rgb}\\left("\
-                            + str(int(255 * color[0])) + ","\
-                            + str(int(255 * color[1])) + ","\
-                            + str(int(255 * color[2])) + "\\right),"
-    result_dict["c"] = result_dict["c"][:-1] + "\\right]"
+    if colors_enabled:
+        # use rgb() of list instead of list of rgb() to save space
+        result_dict["c"] = name_parse(obj_name+"c")+"=\\operatorname{rgb}("
+        result_dict["c"] += flatten(str(int(255 * color[0])) for color in face_colors)+","
+        result_dict["c"] += flatten(str(int(255 * color[1])) for color in face_colors)+","
+        result_dict["c"] += flatten(str(int(255 * color[2])) for color in face_colors)
+        result_dict["c"] += ")"
     
     result = ""
     for key in result_dict:
         result += result_dict[key] + "\n"
+
+    if RENDER_HELPER:
+        vname = name_parse(obj_name+"v")
+        fname = name_parse(obj_name+"f")
+        result += "\\operatorname{triangle}\\left("+\
+                    vname+"\\left[x.x\\right],"+\
+                    vname+"\\left[x.y\\right],"+\
+                    vname+"\\left[x.z\\right]\\right)\\operatorname{for}x="+fname
 
     output_file.write(result)
     output_file.close()
